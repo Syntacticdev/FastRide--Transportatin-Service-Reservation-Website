54d7f2435ba1486d6db82bc341c5cf4a57124f52
Syntacticdev <dsyntacticdev@gmail.com>
2022-07-23 06:26:31 +0100

    fast ride

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,9 @@ import Link from "next/link"
 const Header = () => {
   return (
     <div className='px-6 py-4 sticky bg-white top-0 w-screen flex items-center justify-between'>
-      <a href="/" className="text-logo"><span className='text-3xl font-bold text-red-600'>Fast</span><span className='text-4xl italic font-semibold underline'>Ride</span></a>
+      <Link href="/">
+        <a className="text-logo"><span className='text-3xl font-bold text-red-600'>Fast</span><span className='text-4xl italic font-semibold underline'>Ride</span></a>
+      </Link>
 
       <div className="actions">
         <div className="socials">
